98c4247ffe2b3fddc2060d5dcd363e5d41324cc5
Arunava Chakraborty <144585145+Codewith-Arunava@users.noreply.github.com>
2026-06-28 23:11:23 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -5,7 +5,7 @@ const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: '30d',
   });
-}; 
+};  
 // @desc    Register a new user 
 // @access  Public
 const registerUser = async (req, res) => {
